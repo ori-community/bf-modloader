87e20124ebddf3468a36cb8faa8202c85a707a68
Kirefel <kirefelan@gmail.com>
2022-04-18 20:12:36 +1000

    Nuget publish

--- publishnuget.ps1
+++ publishnuget.ps1
@@ -1,4 +1,8 @@
+param([Parameter(Mandatory)] [string] $ApiKey)
+
 cp build\OriDeModLoader.dll package\lib\net35\
 cp build\Assembly-CSharp.dll package\lib\net35\
 cp build\UnityEngine.dll package\lib\net35\
 nuget pack package -OutputDirectory pkgbuild
+
+nuget push pkgbuild\*.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey $ApiKey
