a19ee4d37fcee580466339f4385dbf433922a92c
Kirefel <kirefelan@gmail.com>
2022-04-25 21:34:26 +1000

    Workflow dispatch

--- publishnuget.ps1
+++ publishnuget.ps1
@@ -1,10 +1,13 @@
-param([Parameter(Mandatory)] [string] $ApiKey)
+param (
+    [Parameter(Mandatory)] [string] $ApiKey,
+    [Parameter(Mandatory)] [string] $Version
+)
 
 mkdir .\package\lib\net35 -ErrorAction SilentlyContinue | Out-Null
 
 cp build\OriDeModLoader.dll package\lib\net35\
 cp build\Assembly-CSharp.dll package\lib\net35\
 cp build\UnityEngine.dll package\lib\net35\
-nuget pack package -OutputDirectory pkgbuild
+nuget pack package -OutputDirectory pkgbuild -Properties PackageVersion=$($Version.trimstart("v"))
 
 nuget push pkgbuild\*.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey $ApiKey
