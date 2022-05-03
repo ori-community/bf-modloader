f69a1aae5d1931697dd70a91445887bdb377ad6d
Kirefel <kirefelan@gmail.com>
2022-05-03 21:29:05 +1000

    Update artifact build for minject

--- build.ps1
+++ build.ps1
@@ -1,1 +1,11 @@
+rm -R .\build -ErrorAction SilentlyContinue
+rm -R .\release -ErrorAction SilentlyContinue
+
 msbuild /t:Build /restore /p:Configuration=Release /p:OutDir=..\build
+dotnet publish Injector/Injector.csproj -c Release -o release
+
+cp .\build\OriDeModLoader.dll .\release\
+cp .\build\0Harmony.dll .\release\
+
+rm .\OriDeModLoader.zip -ErrorAction SilentlyContinue
+Compress-Archive .\release\* .\OriDeModLoader.zip
--- publishrelease.ps1
+++ publishrelease.ps1
@@ -4,8 +4,6 @@ mkdir .\release | Out-Null
 
 cp .\build\OriDeModLoader.dll .\release\
 cp .\build\0Harmony.dll .\release\
-cp .\winhttp.dll .\release\
-cp .\doorstop_config.ini .\release\
 
 rm .\OriDeModLoader.zip -ErrorAction SilentlyContinue
 Compress-Archive .\release\* .\OriDeModLoader.zip
