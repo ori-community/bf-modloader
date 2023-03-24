84bb4dc0821df8d538e6c690bfbdb9cd95f98c8e
Kirefel <kirefelan@gmail.com>
2023-03-24 20:54:49 +1100

    Don't auto-select first item in dropdowns

---
+++ OriDeModLoader/UIExtensions/Menu/DontAutoSelectFirstItemInDropdowns.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace OriDeModLoader.UIExtensions
+{
+    [HarmonyPatch(typeof(CleverMenuItemSelectionManager), nameof(CleverMenuItemSelectionManager.SetIndexToFirst))]
+    static class DontAutoSelectFirstItemInDropdowns
+    {
+        static bool Prefix(CleverMenuItemSelectionManager __instance)
+        {
+            // Don't auto select item 0 because then we can't auto select what we want it to start as
+            // Built in options (i.e. resolution and language) already set themselves to 0 anyway when opened
+            if (__instance.GetComponent<CleverMenuOptionsList>())
+                return HarmonyHelper.StopExecution;
+            return HarmonyHelper.ContinueExecution;
+        }
+    }
+}
